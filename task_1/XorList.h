f7e66fd6adf2a939540e24befdaf3e9f748bd2fa
farhit1 <32172892+farhit1@users.noreply.github.com>
2018-04-30 17:30:02 +0300

    Update XorList.h

--- task_1/XorList.h
+++ task_1/XorList.h
@@ -30,191 +30,73 @@ class XorList {
     typename Allocator::template rebind<Node>::other    _nodeAllocator;
 
     template<typename U>
-    Node* _getNode(U&& value) {
-        Node* newNode = _nodeAllocator.allocate(1);
-        _nodeAllocator.construct(newNode, std::forward<U>(value), _front);
-        return newNode;
-    }
+    Node* _getNode(U&& value);
 
-    void _removeNode(Node* node) {
-        _nodeAllocator.destroy(node);
-        _nodeAllocator.deallocate(node, 1);
-    }
+    void _removeNode(Node* node);
 
 public:
-    explicit XorList(const Allocator& alloc = Allocator()) :
-            _front(nullptr),
-            _back(nullptr),
-            _size(0),
-            _allocator(alloc) {}
+    explicit XorList(const Allocator& alloc = Allocator());
+
     explicit XorList(size_t count,
                      const value_type& value = value_type(),
-                     const Allocator& alloc = Allocator()) :
-            XorList(alloc) {
-        while (count--)
-            push_back(value);
-    }
+                     const Allocator& alloc = Allocator());
 
 
-    size_t size() const {
-        return _size;
-    }
+    size_t size() const;
 
-    bool empty() const {
-        return _size == 0;
-    }
+    bool empty() const;
 
-    void clear() {
-        while (!empty())
-            pop_back();
-    }
+    void clear();
 
 
-    XorList& operator=(const XorList& other) {
-        if (&other == this)
-            return *this;
-        clear();
-        for (auto it = other.begin(); it != other.end(); ++it)
-            push_back(*it);
-        return *this;
-    }
+    XorList& operator=(const XorList& other);
 
-    XorList& operator=(XorList&& other) noexcept {
-        if (&other == this)
-            return *this;
-        _front = std::move(other._front);
-        _back = std::move(other._back);
-        _size = std::move(other._size);
-        _allocator = std::move(other._allocator);
-        return *this;
-    }
+    XorList& operator=(XorList&& other) noexcept;
 
-    XorList(const XorList& other) :
-            XorList() {
-        operator=(other);
-    }
+    XorList(const XorList& other);
 
-    XorList(const XorList&& other) noexcept {
-        operator=(std::move(other));
-    }
+    XorList(const XorList&& other) noexcept;
 
-    ~XorList() {
-        clear();
-    }
+    ~XorList();
 
 
     template<typename U>
-    void push_front(U&& value) {
-        Node* newNode = _getNode(std::forward<U>(value));
-        newNode->xor_addr = _front;
-        if (_front != nullptr) {
-            _front->xor_addr = _xor(_front->xor_addr, newNode);
-            _front = newNode;
-        } else
-            _front = _back = newNode;
-        ++_size;
-    }
+    void push_front(U&& value);
 
     template<typename U>
-    void push_back(U&& value) {
-        Node* newNode = _getNode(std::forward<U>(value));
-        newNode->xor_addr = _back;
-        if (_back != nullptr) {
-            _back->xor_addr = _xor(_back->xor_addr, newNode);
-            _back = newNode;
-        } else
-            _front = _back = newNode;
-        ++_size;
-    }
+    void push_back(U&& value);
 
-    void pop_front() {
-        if (_size == 1)
-            _front = _back = nullptr;
-        else {
-            Node* newFront = reinterpret_cast<Node*>(_front->xor_addr);
-            newFront->xor_addr = _xor(newFront->xor_addr, _back);
-            _removeNode(_front);
-            _front = newFront;
-        }
-        --_size;
-    }
+    void pop_front();
 
-    void pop_back() {
-        if (_size == 1)
-            _front = _back = nullptr;
-        else {
-            Node* newBack = reinterpret_cast<Node*>(_back->xor_addr);
-            newBack->xor_addr = _xor(newBack->xor_addr, _back);
-            _removeNode(_back);
-            _back = newBack;
-        }
-        --_size;
-    }
+    void pop_back();
 
 
     class Iterator {
         Node* _prevNode;
         Node* _node;
-        Node* _nextNode;
+
+        Node* _getNextNode() const;
 
     public:
-        Iterator(Node* prevNode, Node* node, Node* nextNode) :
-                _prevNode(prevNode),
-                _node(node),
-                _nextNode(nextNode) {}
+        Iterator(Node* prevNode, Node* node);
 
-        Iterator& operator=(const Iterator& iterator) {
-            _prevNode = iterator._prevNode;
-            _node = iterator._node;
-            _nextNode = iterator._nextNode;
-            return *this;
-        }
+        Iterator& operator=(const Iterator& iterator);
 
-        Iterator& operator++() {
-            Node* newNextNode = _xor(_nextNode == nullptr ? nullptr : _nextNode->xor_addr,
-                                     _node);
-            _prevNode = _node;
-            _node = _nextNode;
-            _nextNode = newNextNode;
-            return *this;
-        }
+        Iterator& operator++();
 
-        Iterator operator++(int) {
-            Iterator tmp(*this);
-            operator++();
-            return tmp;
-        }
+        Iterator operator++(int);
 
-        Iterator& operator--() {
-            Node* newPrevNode = _xor(_prevNode == nullptr ? nullptr : _prevNode->xor_addr,
-                                     _node);
-            _nextNode = _node;
-            _node = _prevNode;
-            _prevNode = newPrevNode;
-            return *this;
-        }
+        Iterator& operator--();
 
-        Iterator operator--(int) {
-            Iterator tmp(*this);
-            operator--();
-            return tmp;
-        }
+        Iterator operator--(int);
 
-        value_type& operator*() const {
-            return _node->value;
-        }
+        value_type& operator*() const;
 
-        value_type* operator->() const {
-            return _node;
-        }
+        value_type* operator->() const;
 
-        bool operator==(const Iterator& other) const {
-            return _node == other._node;
-        }
+        bool operator==(const Iterator& other) const;
 
-        bool operator!=(const Iterator& other) const {
-            return !operator==(other);
-        }
+        bool operator!=(const Iterator& other) const;
 
         ~Iterator() = default;
 
@@ -222,88 +104,23 @@ public:
         friend class XorList;
     };
 
-    Iterator begin() const {
-        return Iterator(nullptr, _front,
-                        _front == nullptr ? nullptr : reinterpret_cast<Node*>(_front->xor_addr));
-    }
+    Iterator begin() const;
 
-    Iterator end() const {
-        return Iterator(_back, nullptr, nullptr);
-    }
+    Iterator end() const;
 
     template<typename U>
-    void insert_before(Iterator iterator, U&& value) {
-        Node* newNode = _getNode(std::forward<U>(value));
-        newNode->xor_addr = _xor(iterator._prevNode, iterator._node);
-        if (empty())
-            _front = _back = newNode;
-        if (iterator._node != nullptr) {
-            iterator._node->xor_addr = _xor(iterator._node->xor_addr, iterator._prevNode);
-            iterator._node->xor_addr = _xor(iterator._node->xor_addr, newNode);
-        }
-        if (iterator._prevNode != nullptr) {
-            iterator._prevNode->xor_addr = _xor(iterator._prevNode->xor_addr, iterator._node);
-            iterator._prevNode->xor_addr = _xor(iterator._prevNode->xor_addr, newNode);
-        }
-        if (iterator._node == _front)
-            _front = newNode;
-        if (iterator._prevNode == _back)
-            _back = newNode;
-        ++_size;
-    }
+    void insert_before(Iterator iterator, U&& value);
 
     template<typename U>
-    void insert_after(Iterator iterator, U&& value) {
-        Node* newNode = _getNode(std::forward<U>(value));
-        newNode->xor_addr = _xor(iterator._nextNode, iterator._node);
-        if (empty())
-            _front = _back = newNode;
-        if (iterator._node != nullptr) {
-            iterator._node->xor_addr = _xor(iterator._node->xor_addr, iterator._nextNode);
-            iterator._node->xor_addr = _xor(iterator._node->xor_addr, newNode);
-        }
-        if (iterator._nextNode != nullptr) {
-            iterator._nextNode->xor_addr = _xor(iterator._nextNode->xor_addr, iterator._node);
-            iterator._nextNode->xor_addr = _xor(iterator._nextNode->xor_addr, newNode);
-        }
-        if (iterator._nextNode == _front)
-            _front = newNode;
-        if (iterator._node == _back)
-            _back = newNode;
-        ++_size;
-    }
+    void insert_after(Iterator iterator, U&& value);
 
-    void erase(Iterator iterator) {
-        --_size;
-        if (iterator._prevNode != nullptr) {
-            iterator._prevNode->xor_addr = _xor(iterator._prevNode->xor_addr, iterator._node);
-            iterator._prevNode->xor_addr = _xor(iterator._prevNode->xor_addr, iterator._nextNode);
-        }
-        if (iterator._nextNode != nullptr) {
-            iterator._nextNode->xor_addr = _xor(iterator._nextNode->xor_addr, iterator._node);
-            iterator._nextNode->xor_addr = _xor(iterator._nextNode->xor_addr, iterator._prevNode);
-        }
-        if (_front == iterator._node)
-            _front = iterator._nextNode;
-        if (_back == iterator._node)
-            _back = iterator._prevNode;
-        _removeNode(iterator._node);
-    }
+    void erase(Iterator iterator);
 
 #if __has_include("vector")
-    std::vector<value_type> toVector() const {
-        std::vector<value_type> tmp;
-        tmp.reserve(size());
-        for (auto it = begin(); it != end(); ++it)
-            tmp.push_back(*it);
-        return tmp;
-    }
-
-    operator std::vector<value_type>() const {
-        return toVector();
-    }
+    operator std::vector<value_type>() const;
 #endif
 };
+
 
 #include "XorList.cpp"
 
